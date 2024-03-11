441968758d19460e277b82e2c797efb87d417824
Gustavo Henrique dos Reis <61255621+Reisgth@users.noreply.github.com>
2024-03-11 08:22:07 -0300

    Update Aplic.java

--- prjRetangulo1/src/Aplic.java
+++ prjRetangulo1/src/Aplic.java
@@ -3,20 +3,34 @@
  * @author GustavoReis
  */
 public class Aplic {
-
     public static void main(String[] args) {
-        Retangulo objRet1;
+        Retangulo objRet1; //definição do ponteiro
+        Retangulo objRet2;
         
-        objRet1 = new Retangulo();
+        objRet1 = new Retangulo();//Instanciação do objeto
+                                  //da classe Retangulo
+             
+        objRet2 = new Retangulo();
         
         objRet1.setAltura(5.0);
-        objRet1.setBase(8.0);
-            
-        System.out.println("Valor da Altura: " + objRet1.getAltura());
-        System.out.println("Valor do Base: " + objRet1.getBase());
-        System.out.println("Valor da area: " + objRet1.calcArea());
-        System.out.println("Valor do Perimetro: " + objRet1.calcPerimetro());
+        objRet1.setBase(8.0);        
+        
+        System.out.println("Objeto Ret1\n");
+        System.out.println("Medida da altura: " + objRet1.getAltura());
+        System.out.println("Medida da base: " + objRet1.getBase());
+        System.out.println("Valor da Área: " + objRet1.calcArea());
+        System.out.println("Valor do Perímetro: " + objRet1.calcPerimetro());
         System.out.println("Valor da Diagonal: " + objRet1.calcDiagonal());
-    }
     
+    
+        objRet2.setAltura(3.0);
+        objRet2.setBase(4.0);
+        
+        System.out.println("\n\nObjeto Ret2\n");
+        System.out.println("Medida da altura: " + objRet2.getAltura());
+        System.out.println("Medida da base: " + objRet2.getBase());
+        System.out.println("Valor da Área: " + objRet2.calcArea());
+        System.out.println("Valor do Perímetro: " + objRet2.calcPerimetro());
+        System.out.println("Valor da Diagonal: " + objRet2.calcDiagonal());
+    }
 }
